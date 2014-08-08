67e52274b72c42a722840384389c9cff958c6e07
Scott Haefner <shaefner@usgs.gov>
2014-08-08 11:05:18 -0700

    move db config inside app directory

---
+++ conf/db.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+// db params
+define('HOSTNAME', '');
+define('DATABASE', '');
+define('USERNAME', '');
+define('PASSWORD', '');
+
+// db connect
+$db = mysql_connect(HOSTNAME, USERNAME, PASSWORD) or die(mysql_error());
+mysql_select_db(DATABASE, $db);
+
+?>

82c09325d4a8ab2d59d8cde71a9b76badfc934fd
Scott Haefner <shaefner@usgs.gov>
2014-08-20 16:54:47 -0700

    add comments

--- conf/conf.inc.php
+++ conf/conf.inc.php
@@ -1,15 +1,23 @@
 <?php
 
+/* 
+ * Configuration params - feel free to change these
+ */
+
 // ImageMagick's convert cmd
 $convert = '/usr/bin/convert';
 
 // Maximum upload filesize
 $maxsize = 2000000;
 
+/* 
+ * Params used by multiple files in app 
+ */
+
 // set time zone for recorded / synced db fields to UTC (timestamp from device's geolocation is stored in localtime)
 date_default_timezone_set('UTC');
 
-// Feature forms - names and associated db tables
+// Feature forms - names and associated db tables (must match form name values in index.html)
 $tables = array(
 	'landslide' => 'Landslide',
 	'liquefaction' => 'Liquefaction',
